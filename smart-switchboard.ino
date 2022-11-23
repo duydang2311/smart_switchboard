f6c007383eb04d67a87cea9ba0590718f712375d
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-24 01:53:39 +0700

    Remove boilerplate code.

--- smart-switchboard.ino
+++ smart-switchboard.ino
@@ -14,28 +14,19 @@ char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "";
-char pass[] = "";
+char ssid[] = "Quang Huy";
+char pass[] = "hettienroiban";
 
 BlynkTimer timer;
 
 BLYNK_WRITE(V0)
 {
   int value = param.asInt();
-
-  Blynk.virtualWrite(V1, value);
+  digitalWrite(0, value);
 }
 
 BLYNK_CONNECTED()
 {
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
-}
-
-void myTimerEvent()
-{
-  Blynk.virtualWrite(V2, millis() / 1000);
 }
 
 void setup()
@@ -43,11 +34,9 @@ void setup()
   Serial.begin(115200);
 
   Blynk.begin(auth, ssid, pass);
-  timer.setInterval(1000L, myTimerEvent);
 }
 
 void loop()
 {
   Blynk.run();
-  timer.run();
 }
